12e33cb413b7ec4ed94c5fd8c5eea8c1cf36fa6b
Ruifeng Zhang <ruifeng.zhang@cbb.de>
2024-01-24 09:28:46 +0100

    add upload_my_hcp.ps1

---
+++ upload_my_hcp.ps1
@@ -0,0 +1,33 @@
+param(
+  [string]$hostNo
+)
+
+$password = Read-Host -Prompt "Enter your password"
+
+$ScriptDirectory = $PSScriptRoot
+
+
+
+& "D:\OneDrive - cbb software GmbH\RuifengZhang\Apps\Winscp\WinSCP-5.19.6-Portable\WinSCP.com" `
+  /log="./WinSCP.log" /ini=nul `
+  /command `
+    "open scp://pi:$password@192.168.1.23/ -hostkey=* " `
+    "lcd `"$ScriptDirectory`"" `
+    "cd /home/pi/hcp/test" `
+    "rm *" `
+    "put *.py -transfer=binary"  `
+	"put *.sh -transfer=binary"  `
+    "exit"
+
+$winscpResult = $LastExitCode
+if ($winscpResult -eq 0)
+{
+  Write-Host "Success"
+  Remove-item ".\WinSCP.log"
+}
+else
+{
+  Write-Host "Error"
+}
+
+exit $winscpResult
